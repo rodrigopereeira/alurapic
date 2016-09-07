d987b25076df8bc11c04bbb813b9f78ab85bf9ed
Rodrigo Pereira <rpereira1415@gmail.com>
2016-09-07 00:08:55 -0300

    ¨config_mongo¨

--- app/routes/foto.js
+++ app/routes/foto.js
@@ -3,14 +3,6 @@ module.exports = function(app) {
     var api = app.api.foto;
 
     // passa a função api.lista como parâmetro
-    app.route('/v1/fotos')
-        .get(api.lista)
-        .post(api.adiciona);
-
-    //mesma url com protocolos diferentes
-    app.route('/v1/fotos/:id')
-        .get(api.buscaPorId)
-        .delete(api.removePorId)
-        .put(api.atualiza);
+    app.get('/v1/fotos', api.lista);
 
 };
--- config/express.js
+++ config/express.js
@@ -1,11 +1,9 @@
 var express = require("express");
 var consign = require('consign');
-var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(express.static('./public'));
-app.use(bodyParser.json());
 
 //inserindo modulos de api e rotas pelo consign e passando o servidor como parametro
 consign({
